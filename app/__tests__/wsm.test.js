3ff76b69b649212b45ae7a7c13d65a0d079ef1d1
Upkar Lidder <lidderupk@gmail.com>
2017-06-01 10:12:02 -0700

    --amend

--- app/__tests__/wsm.test.js
+++ app/__tests__/wsm.test.js
@@ -1,5 +1,5 @@
-const wdm = require('./wsm.js');
+const wsm = require('../wsm.js');
 
 test('wsm module should export a function', () => {
     expect(typeof wsm).toBe('function');
-})
+});
---
+++ app/wsm.js
@@ -0,0 +1,5 @@
+function wsm() {
+
+}
+console.log(`typeof wsm: ${typeof wsm}`);
+module.exports = wsm;
